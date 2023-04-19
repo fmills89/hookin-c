eea5ba341e95c7eb46d7e14a6a1d0a8a66e7de87
FoboIX <mills_forrest@yahoo.com>
2023-04-19 15:06:23 -0500

    removed console.log

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
 import Work from "./components/Work/Work";
 
-console.log(process.env.REACT_APP_NODEMAILER_PASS);
-
 const style = {
   zindex40: `z-40`,
   zindex30: `z-30`,
